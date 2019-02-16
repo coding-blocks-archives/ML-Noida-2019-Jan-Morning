e8758625d296f451e3a6d537098ef18d06b199b0
Keenarrior <anujgargcse@gmail.com>
2019-02-16 15:35:25 +0530

    l-5

--- lecture_5/pycharm/detection.py
+++ lecture_5/pycharm/detection.py
@@ -1,14 +1,23 @@
 import cv2
+import numpy as np
+
+import np_writer
+
+name = input("Enter your name : ")
 
 cap = cv2.VideoCapture(0)
 
 classifier = cv2.CascadeClassifier("../../dataset/haarcascade_frontalface_default.xml")
 
+f_list = []
+
 while True:
 
     ret, frame = cap.read()
 
-    faces = classifier.detectMultiScale(frame, 1.5, 5)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = classifier.detectMultiScale(gray, 1.5, 5)
 
     faces = sorted(faces, key=lambda f: f[2]*f[3], reverse=True)
 
@@ -21,8 +30,6 @@ while True:
         cv2.imshow("face", im_face)
 
 
-    print(len(faces))
-
     # if not ret:
     #     continue
 
@@ -32,6 +39,21 @@ while True:
 
     if key & 0xFF == ord('q'):
         break
+    elif key & 0xFF == ord('c'):
+        if len(faces) == 1:
+            gray_face = cv2.cvtColor(im_face, cv2.COLOR_BGR2GRAY)
+            gray_face = cv2.resize(gray_face, (100, 100))
+            print(len(f_list), type(gray_face), gray_face.shape)
+            f_list.append(gray_face.reshape(-1))
+        else:
+            print("face not found")
 
+        if len(f_list) == 10:
+            break
+
+
+np_writer.write(name, np.array(f_list))
+
+# np.save(name, np.array(f_list))
 
 cap.release()
---
+++ lecture_5/pycharm/recog.py
@@ -0,0 +1,66 @@
+import cv2
+import numpy as np
+
+from np_writer import f_name
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.neighbors import KNeighborsClassifier
+
+data = pd.read_csv(f_name).values
+
+X, Y = data[:, 1:-1], data[:, -1]
+
+print(X.shape, Y.shape)
+
+model = KNeighborsClassifier(n_neighbors=4)
+
+model.fit(X, Y)
+
+cap = cv2.VideoCapture(0)
+
+classifier = cv2.CascadeClassifier("../../dataset/haarcascade_frontalface_default.xml")
+
+f_list = []
+
+while True:
+
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = classifier.detectMultiScale(gray, 1.5, 5)
+
+    X_test = []
+
+    for face in faces:
+        x, y, w, h = face
+        im_face = gray[y:y+h, x:x+w]
+        im_face = cv2.resize(im_face, (100, 100))
+        X_test.append(im_face.reshape(-1))
+        # cv2.imshow("face", im_face)
+
+    if len(faces) > 0:
+        response = model.predict(np.array(X_test))
+
+        for i, face in enumerate(faces):
+            x, y, w, h = face
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 5)
+            cv2.putText(frame, response[i], (x-50, y-50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 255), 2)
+
+    # if not ret:
+    #     continue
+
+    cv2.imshow("full", frame)
+
+    key = cv2.waitKey(1)
+
+    if key & 0xFF == ord('q'):
+        break
+
+
+
+# np.save(name, np.array(f_list))
+
+cap.release()
